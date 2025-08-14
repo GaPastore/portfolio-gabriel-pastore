8fbc8d46014ab58201434b7afc6426e1d1be8514
Gabriel Pastore <79768975+GaPastore@users.noreply.github.com>
2025-08-14 15:12:10 -0300

    style: melhora pagina de contato + corrige problemas do mobile

--- src/components/Audiovisual/ContentAudiovisual.js
+++ src/components/Audiovisual/ContentAudiovisual.js
@@ -1,15 +1,29 @@
-import React, { useState } from 'react';
 import styles from '../../styles/components/ContentLinks.module.css';
+import { useTranslation } from "react-i18next";
 
-function Header(){
+function ContentAudiovisual(){
+
+    const { t } = useTranslation();
 
     return(
         <div>
             <div className={styles.introDiv}>
                     <div className={styles.flexDiv}>
-                    <div className={styles.drawImg} id={styles.somosAlgo}><a className={styles.imgBox + " oswald-geral"} href='https://instagram.com/curtasomosalgo' target='_blank' rel="noopener noreferrer">Somos Algo? - Marketing Digital para curta 🔗</a></div>
-                    <div className={styles.drawImg} id={styles.fitTrack}><a className={styles.imgBox + " oswald-geral"} href='https://www.figma.com/proto/zxMUrCh7GsqKeHIQN0Pg5l?node-id=0-1&t=otN8lK9XmDXJ5hbo-6' target='_blank' rel="noopener noreferrer">FitTrack - Projeto visual de App Mobile🔗</a></div>
-                    <div className={styles.drawImg} id={styles.youtube}><a className={styles.imgBox + " oswald-geral"} href='https://youtube.com/@GaPastore' target='_blank' rel="noopener noreferrer">Pastore - Canal do YouTube 🔗</a></div>
+                    <div className={styles.drawImg} id={styles.somosAlgo}>
+                        <a className={styles.imgBox + " oswald-geral"} href='https://instagram.com/curtasomosalgo' target='_blank' rel="noopener noreferrer">
+                            {t("audiovisual.somosAlgo")}
+                        </a>
+                    </div>
+                    <div className={styles.drawImg} id={styles.fitTrack}>
+                        <a className={styles.imgBox + " oswald-geral"} href='https://www.figma.com/proto/zxMUrCh7GsqKeHIQN0Pg5l?node-id=0-1&t=otN8lK9XmDXJ5hbo-6' target='_blank' rel="noopener noreferrer">
+                            {t("audiovisual.fitTrack")}
+                        </a>
+                    </div>
+                    <div className={styles.drawImg} id={styles.youtube}>
+                        <a className={styles.imgBox + " oswald-geral"} href='https://youtube.com/@GaPastore' target='_blank' rel="noopener noreferrer">
+                            {t("audiovisual.youtube")}
+                        </a>
+                    </div>
                 </div>
             </div>
         </div>
@@ -17,4 +31,4 @@ function Header(){
 
 }
 
-export default Header;
+export default ContentAudiovisual;
--- src/components/CienciaDaComputacao/ContentCC.js
+++ src/components/CienciaDaComputacao/ContentCC.js
@@ -1,20 +1,54 @@
-import React, { useState } from 'react';
 import styles from '../../styles/components/ContentLinks.module.css';
+import { useTranslation } from "react-i18next";
 
-function Header(){
+function ContentCC(){
+
+    const { t } = useTranslation();
 
     return(
         <div>
             <div className={styles.introDiv}>
                 <div className={styles.flexDiv}>
-                <div className={styles.drawImg} id={styles.chronolight}><a className={styles.imgBox + " oswald-geral"} href='https://mutoustudios.itch.io/chronolight' target='_blank' rel="noopener noreferrer">ChronoLight - Jogo 3D 🔗</a></div>
-                    <div className={styles.drawImg} id={styles.micescape}><a className={styles.imgBox + " oswald-geral"} href='https://mutoustudios.itch.io/micescape' target='_blank' rel="noopener noreferrer">Micescape - Jogo 2D 🔗</a></div>
-                    <div className={styles.drawImg} id={styles.iconomonster}><a className={styles.imgBox + " oswald-geral"} href='https://github.com/GaPastore/a3-icon-generator/tree/main' target='_blank' rel="noopener noreferrer">Iconomonster - Site em Vue.js 🔗</a></div>
-                    <div className={styles.drawImg} id={styles.imageasy}><a className={styles.imgBox + " oswald-geral"} href='https://github.com/GiuliaNogoliver/estruturas_matematicas/tree/branch-final' target='_blank' rel="noopener noreferrer">ImagEasy - Site em Angular 🔗</a></div>
-                    <div className={styles.drawImg} id={styles.curriculumAI}><a className={styles.imgBox + " oswald-geral"} href='https://github.com/Curriculum-Vitae-AI' target='_blank' rel="noopener noreferrer">Curriculum AI - Site em React 🔗</a></div>
-                    <div className={styles.drawImg} id={styles.greenPrint}><a className={styles.imgBox + " oswald-geral"} href='https://github.com/GaPastore/A3-Java' target='_blank' rel="noopener noreferrer">Green Print - App em Java 🔗</a></div>
-                    <div className={styles.drawImg} id={styles.equacionando}><a className={styles.imgBox + " oswald-geral"} href='https://gapastore.itch.io/equacionando' target='_blank' rel="noopener noreferrer">Equacionando² - Jogo 2D educacional 🔗</a></div>
-                    <div className={styles.drawImg} id={styles.theDomeOfHope}><a className={styles.imgBox + " oswald-geral"} href='https://gapastore.itch.io/the-dome-of-hope' target='_blank' rel="noopener noreferrer">The Dome of Hope - Jogo 2D educacional 🔗</a></div>
+                    <div className={styles.drawImg} id={styles.chronolight}>
+                        <a className={styles.imgBox + " oswald-geral"} href='https://mutoustudios.itch.io/chronolight' target='_blank' rel="noopener noreferrer">
+                            {t("computerScience.chronolight")}
+                        </a>
+                    </div>
+                    <div className={styles.drawImg} id={styles.micescape}>
+                        <a className={styles.imgBox + " oswald-geral"} href='https://mutoustudios.itch.io/micescape' target='_blank' rel="noopener noreferrer">
+                            {t("computerScience.micescape")}
+                        </a>
+                    </div>
+                    <div className={styles.drawImg} id={styles.iconomonster}>
+                        <a className={styles.imgBox + " oswald-geral"} href='https://github.com/GaPastore/a3-icon-generator/tree/main' target='_blank' rel="noopener noreferrer">
+                            {t("computerScience.iconomonster")}
+                        </a>
+                    </div>
+                    <div className={styles.drawImg} id={styles.imageasy}>
+                        <a className={styles.imgBox + " oswald-geral"} href='https://github.com/GiuliaNogoliver/estruturas_matematicas/tree/branch-final' target='_blank' rel="noopener noreferrer">
+                            {t("computerScience.imageasy")}
+                        </a>
+                    </div>
+                    <div className={styles.drawImg} id={styles.curriculumAI}>
+                        <a className={styles.imgBox + " oswald-geral"} href='https://github.com/Curriculum-Vitae-AI' target='_blank' rel="noopener noreferrer">
+                            {t("computerScience.curriculumAI")}
+                        </a>
+                    </div>
+                    <div className={styles.drawImg} id={styles.greenPrint}>
+                        <a className={styles.imgBox + " oswald-geral"} href='https://github.com/GaPastore/A3-Java' target='_blank' rel="noopener noreferrer">
+                            {t("computerScience.greenPrint")}
+                        </a>
+                    </div>
+                    <div className={styles.drawImg} id={styles.equacionando}>
+                        <a className={styles.imgBox + " oswald-geral"} href='https://gapastore.itch.io/equacionando' target='_blank' rel="noopener noreferrer">
+                            {t("computerScience.equacionando")}
+                        </a>
+                    </div>
+                    <div className={styles.drawImg} id={styles.theDomeOfHope}>
+                        <a className={styles.imgBox + " oswald-geral"} href='https://gapastore.itch.io/the-dome-of-hope' target='_blank' rel="noopener noreferrer">
+                            {t("computerScience.theDomeOfHope")}
+                        </a>
+                    </div>
                 </div>
             </div>
         </div>
@@ -22,4 +56,4 @@ function Header(){
 
 }
 
-export default Header;
+export default ContentCC;
