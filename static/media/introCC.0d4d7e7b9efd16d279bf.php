b161c66c6cefd4c91d2f70e8ab42c5ad3cca6fd6
Gabriel Pastore <79768975+GaPastore@users.noreply.github.com>
2025-08-19 15:06:32 -0300

    Updates

---
+++ static/media/introCC.0d4d7e7b9efd16d279bf.php
@@ -0,0 +1,44 @@
+Tenho como foco de estudos a tecnologia desde 2018, quando ingressei no Colégio ENIAC e realizei o 
+<strong>ensino técnico</strong> em Informática. Em 2021, ao finalizar a escola, iniciei o <strong>bacharelado</strong> 
+em Ciência da Computação na Universidade São Judas Tadeu, cumprindo os estudos até Junho de 2025. 
+Durante todos esses anos, pude realizar diversos projetos e me aprofundar nas <strong>linguagens de 
+programação</strong>, passando por desenvolvimento web, jogos e aplicações.
+<br><br>
+<u>Habilidades</u>
+<br><br>
+<dl>
+    <dt>HTML e CSS</dt>
+        <dd>Desenvolvimento web com HTML/CSS básico</dd>
+    <br>
+    <dt>Frameworks Web</dt>
+        <dd>Uso de React, Angular e Vue (este site foi desenvolvido em React, por exemplo)</dd>
+    <br>
+    <dt>Desenvolvimento de jogos</dt>
+        <dd>Uso de C# para Unity, além de ter contato prévio com Construct, Java e GameMaker</dd>
+    <br>
+    <dt>Java</dt>
+        <dd>Desenvolvimento de aplicações em Java</dd>
+</dl>
+
+<style>
+    
+    @media (hover:hover) {
+        dl {
+            padding: 0;
+            margin: 0;
+            list-style-type: none;
+        }
+        dd {
+            margin-left: 0;
+            padding-left: 0;
+            font-size: 3vh;
+        }
+    }
+
+    @media (any-pointer: coarse) {
+        dd {
+            font-size: 3vw;
+        }
+    }
+    
+</style>
