3635b0969e39e0d80c7f1ab7a14a1aeec219109e
Gabriel Pastore <79768975+GaPastore@users.noreply.github.com>
2026-08-06 13:25:46 -0300

    feat: migrate from react-router-dom to @tanstack/react-router for better security and maintainability\n\n- Remove react-router-dom dependency (had vulnerability warning)\n- Add @tanstack/react-router v1.43.0 (actively maintained, secure)\n- Update App.jsx to use TanStack Router's modern API\n- Maintain all existing routes and basepath configuration\n- Keep i18n language persistence functionality"

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,94 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { useEffect } from 'react';
-import { useTranslation } from "react-i18next";
-import "./i18n/i18n";
-import Home from './pages/Home';
-import Audiovisual from './pages/Audiovisual';
-import CienciaDaComputacao from './pages/CienciaDaComputacao';
-import Desenhos from './pages/Desenhos';
-import Shot from './pages/Shot';
-import Contato from './pages/Contato';
-import Menu from './components/Menu';
+import { createRouter, RootRoute, Route, RouterProvider } from '@tanstack/react-router'
+import { useEffect } from 'react'
+import { useTranslation } from "react-i18next"
+import "./i18n/i18n"
+import Home from './pages/Home'
+import Audiovisual from './pages/Audiovisual'
+import CienciaDaComputacao from './pages/CienciaDaComputacao'
+import Desenhos from './pages/Desenhos'
+import Shot from './pages/Shot'
+import Contato from './pages/Contato'
+import Menu from './components/Menu'
 
-function App() {
+function RootLayout() {
+  const { i18n } = useTranslation()
 
-  
-  const { t, i18n } = useTranslation();
-
-  useEffect(()=>{
-      const savedLang = localStorage.getItem("lang");
-      if (savedLang) {
-          i18n.changeLanguage(savedLang);
-      }
-  },[])
+  useEffect(() => {
+    const savedLang = localStorage.getItem("lang")
+    if (savedLang) {
+      i18n.changeLanguage(savedLang)
+    }
+  }, [])
 
   return (
-    <Router basename="/portfolio-gabriel-pastore">
-      <Menu/>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/audiovisual' element={<Audiovisual />} />
-        <Route path='/ciencia_da_computacao' element={<CienciaDaComputacao />} />
-        <Route path='/desenhos' element={<Desenhos />} />
-        <Route path='/shot' element={<Shot />} />
-        <Route path='/contato' element={<Contato />} />
-      </Routes>
-    </Router>
-  );
-
+    <>
+      <Menu />
+    </>
+  )
 }
 
-export default App;
+const rootRoute = new RootRoute({
+  component: RootLayout,
+})
+
+const homeRoute = new Route({
+  getParentRoute: () => rootRoute,
+  path: '/',
+  component: Home,
+})
+
+const audiovisualRoute = new Route({
+  getParentRoute: () => rootRoute,
+  path: '/audiovisual',
+  component: Audiovisual,
+})
+
+const cienciaRoute = new Route({
+  getParentRoute: () => rootRoute,
+  path: '/ciencia_da_computacao',
+  component: CienciaDaComputacao,
+})
+
+const desenhos Route = new Route({
+  getParentRoute: () => rootRoute,
+  path: '/desenhos',
+  component: Desenhos,
+})
+
+const shotRoute = new Route({
+  getParentRoute: () => rootRoute,
+  path: '/shot',
+  component: Shot,
+})
+
+const contatoRoute = new Route({
+  getParentRoute: () => rootRoute,
+  path: '/contato',
+  component: Contato,
+})
+
+const routeTree = rootRoute.addChildren([
+  homeRoute,
+  audiovisualRoute,
+  cienciaRoute,
+  desenhosRoute,
+  shotRoute,
+  contatoRoute,
+])
+
+const router = createRouter({
+  routeTree,
+  basepath: '/portfolio-gabriel-pastore',
+})
+
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: typeof router
+  }
+}
+
+function App() {
+  return <RouterProvider router={router} />
+}
+
+export default App
